5892b5fdf4323f9691f41ef554652dedefbbf91f
mohammedsaam <78639441+mohammedsaam@users.noreply.github.com>
2021-07-07 15:19:18 +0530

    hello

--- src/Components/Homepage/Banner.jsx
+++ src/Components/Homepage/Banner.jsx
@@ -1,14 +1,25 @@
 
 import Carousel from "react-material-ui-carousel";
 import {bannerData} from '../../Constants/Data'
+import { makeStyles } from "@material-ui/core";
+
+const useStyle = makeStyles({
+
+    image:{
+        width:"100%"
+    }
+})
+
 
 export const Banner = () => {
+
+    const classes = useStyle();
 
        return(
     <Carousel>
         {
             bannerData.map(image =>(
-                <img src={image} />
+                <img src={image} className={classes.image} />
             ))
         }
     </Carousel>
